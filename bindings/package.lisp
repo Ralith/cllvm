01747b617475e0a2bf7417ff703461a212c9bba8
Benjamin Saunders <besaunde@sfu.ca>
2010-07-17 02:07:03 -0700

    Exports for everything bound so far.

--- bindings/package.lisp
+++ bindings/package.lisp
@@ -1,3 +1,15 @@
 (defpackage #:llvm-bindings (:nicknames #:%llvm)
   (:use #:common-lisp #:cffi)
-  (:export))
+  (:export
+   #:context-ref
+   #:module-ref
+   #:type-ref
+   #:type-handle-ref
+   #:value-ref
+   #:basic-block-ref
+   #:builder-ref
+   #:dispose-module
+   #:link-in-jit
+   #:link-in-interpreter
+   #:verifier-failure-action
+   #:verify-module))
