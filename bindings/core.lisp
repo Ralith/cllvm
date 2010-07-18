5bf12b1c9b18f6bee36206f08eebcedbfe85766f
Benjamin Saunders <besaunde@sfu.ca>
2010-07-17 19:38:27 -0700

    Moved binding utility macros to their own file and fixed a few typos

--- bindings/core.lisp
+++ bindings/core.lisp
@@ -215,33 +215,14 @@
 (defcfun (dispose-builder "LLVMDisposeBuilder") :void
   (builder builder-ref))
 
-(defun lispify-name (string)
-  (loop for char across string
-        with last-char-upcased = t
-        with output = ""
-        do (when (and (upper-case-p char)
-                      (not last-char-upcased))
-             (setf output (concatenate 'string output "-")))
-           (setf output (concatenate 'string output (string char)))
-           (setf last-char-upcased (upper-case-p char))
-        finally (return (string-upcase output))))
-(defmacro definstr (c-name &body args)
-  `(defcfun (,(intern (concatenate 'string "BUILD-" (lispify-name c-name))) ,(concatenate 'string "LLVMBuild" c-name)) value-ref
-     (builder builder-ref)
-     ,@args))
-(defmacro defvinstr (c-name &body args)
-  `(definstr ,c-name
-     ,@args
-     (name :string)))
-
 ;;; Terminators
 (definstr "RetVoid")
-(definstr "BuildRet"
+(definstr "Ret"
   (value value-ref))
 (definstr "AggregateRet"
   (values (:pointer value-ref))
   (number :unsigned-int))
-(definstr "BuildBr"
+(definstr "Br"
   (destination basic-block-ref))
 (definstr "CondBr"
   (condition value-ref)
